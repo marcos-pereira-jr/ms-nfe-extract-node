b86b44aeaa1a9b22a088fec0bf66c4d3d4970f26
marcos-pereira-jr <marcos.ant.pereira.jr@gmail.com>
2022-11-19 17:29:11 -0300

    feat(AMQP): Add call to amqp

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
-console.log('Hellow Word!');
+import amqp from 'amqplib';
+
+async function connect() {
+    const msgBuffer = Buffer.from(JSON.stringify({ number: 10 }));
+    try {
+        const connection = await amqp.connect("amqp://localhost:5672");
+        const channel = await connection.createChannel();
+        await channel.assertQueue("number");
+        await channel.sendToQueue("number", msgBuffer);
+        console.log("Sending message to number queue");
+        await channel.close();
+        await connection.close();
+    } catch (ex) {
+        console.error(ex);
+    }
+} connect();
